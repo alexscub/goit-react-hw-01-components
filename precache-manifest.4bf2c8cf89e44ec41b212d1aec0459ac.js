60eaa26fb4d89e3663f836128f6045a7c49b14ce
alexscub <alexscub1@gmail.com>
2019-10-07 23:52:47 +0300

    Updates

---
+++ precache-manifest.4bf2c8cf89e44ec41b212d1aec0459ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c87605542e8636667ed6ab50e6f5d8",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "4afcacf33593d06ecaba",
+    "url": "/goit-react-hw-01-components/static/css/main.6298ec5d.chunk.css"
+  },
+  {
+    "revision": "6214b4957fbeb4384132",
+    "url": "/goit-react-hw-01-components/static/js/2.6feeed5e.chunk.js"
+  },
+  {
+    "revision": "4afcacf33593d06ecaba",
+    "url": "/goit-react-hw-01-components/static/js/main.ff32f01f.chunk.js"
+  },
+  {
+    "revision": "daffbc77bd7db6c6a4c0",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.d56aa54c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alexscub/goit-react-hw-01-components/precache-manifest.e0c01fb4bdb6899afacde5097caa4a52.js"
+  "/goit-react-hw-01-components/precache-manifest.4bf2c8cf89e44ec41b212d1aec0459ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alexscub/goit-react-hw-01-components/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
